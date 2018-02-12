3e3da1ed7aedf32e844a1fd4d27a476243aa1347
Flibielt <36382930+Flibielt@users.noreply.github.com>
2018-02-12 16:15:33 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int CsereOsszeadassal(int &a, int &b);
+int CsereSzorzassal(int &a, int &b);
+
+int main()
+{
+    int a = 100;
+    int b = 50;
+    cout << "a: " << a << endl << "b: " << b <<endl;
+
+    CsereOsszeadassal(a, b);
+    cout << "Csere\n";
+    cout << "a: " << a << endl << "b: " << b <<endl;
+
+    CsereSzorzassal(a,b);
+    cout << "Csere\n";
+    cout << "a: " << a << endl << "b: " << b <<endl;
+
+    return 0;
+}
+
+
+int CsereOsszeadassal(int &a, int &b)
+{
+    a = b + a;
+    b = a - b;
+    a = a - b;
+}
+
+int CsereSzorzassal(int &a, int &b)
+{
+    a = b * a;
+    b = a / b;
+    a = a / b;
+}
